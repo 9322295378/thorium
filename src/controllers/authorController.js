4abf0d5cf447ff3b9ab480d92532331cf89c1851
KomalBansod <komalbansod1999@gmail.com>
2022-03-03 08:59:41 +0530

    Refrence

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -6,10 +6,31 @@ const createAuthor= async function (req, res) {
     res.send({data: authorCreated})
 }
 
-const getAuthorsData= async function (req, res) {
-    let authors = await AuthorModel.find()
-    res.send({data: authors})
-}
-
 module.exports.createAuthor= createAuthor
-module.exports.getAuthorsData= getAuthorsData
+
+
+
+
+
+
+
+
+
+
+
+
+// const AuthorModel= require("../models/authorModel")
+
+// const createAuthor= async function (req, res) {
+//     let author = req.body
+//     let authorCreated = await AuthorModel.create(author)
+//     res.send({data: authorCreated})
+// }
+
+// const getAuthorsData= async function (req, res) {
+//     let authors = await AuthorModel.find()
+//     res.send({data: authors})
+// }
+
+// module.exports.createAuthor= createAuthor
+// module.exports.getAuthorsData= getAuthorsData
