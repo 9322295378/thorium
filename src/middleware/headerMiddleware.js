7e58ddb1651e10e2c83cd849eaf74cdd3feb8bd4
KomalBansod <komalbansod1999@gmail.com>
2022-03-08 00:15:03 +0530

    headerMiddleware

---
+++ src/middleware/headerMiddleware.js
@@ -0,0 +1,15 @@
+
+const freeAppUser = function(req, res, next){
+    let newHeader= req.headers.isfreeappuser;
+    console.log(newHeader)
+    if(newHeader === "true"){
+next()
+    }
+    else{
+        return res.send({Error: "he request is missing a mandatory header"})
+    }
+}
+
+
+module.exports.freeAppUser = freeAppUser
+ 
